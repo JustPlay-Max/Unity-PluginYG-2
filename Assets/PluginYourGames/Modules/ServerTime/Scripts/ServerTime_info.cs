9a328e07f44401e15e2eff06f1ee096df74a5853
MOPE4ok <115535861+MOPE40k@users.noreply.github.com>
2025-09-09 21:43:05 +0300

    Update ServerTime_info.cs
    - Добавлена возможность выбора формата серверного времени для симуляции (получение текущего времени в формате UTC или установить значение времени вручную).

--- Assets/PluginYourGames/Modules/ServerTime/Scripts/ServerTime_info.cs
+++ Assets/PluginYourGames/Modules/ServerTime/Scripts/ServerTime_info.cs
@@ -1,5 +1,6 @@
 ﻿#if UNITY_EDITOR
 using System;
+using UnityEngine;
 using YG.Insides;
 
 namespace YG
@@ -12,7 +13,15 @@ namespace YG
         public partial class ServerTimeSettings
         {
             [HeaderYG(Langs.simulation, 5)]
-            public long serverTime = 1721201231000;
+            public long serverTime => manualTimeSetup ? timeInMs : DateTimeOffset.Now.ToUnixTimeMilliseconds();
+#if RU_YG2
+            [Tooltip("true - возвращает значение времени заданное в поле ниже.\nfalse - возвращает текущую всемирное время.")]
+#else
+            [Tooltip("true - returns the time value specified in the field below.\nfalse - returns the current universal time.")]
+#endif
+            public bool manualTimeSetup = false;
+            [NestedYG(nameof(manualTimeSetup))]
+            public long timeInMs = 1721201231000;
         }
     }
 }
