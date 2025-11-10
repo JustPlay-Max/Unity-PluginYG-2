6797937f9a2b965594352ad365ceede12267cc62
Max Bornysov <mbornysov@mail.ru>
2025-11-10 16:12:58 +0700

    Delete Bonuses module
    Удалил внутренний модуль Bonuses

--- Assets/PluginYourGames/Modules/Bonuses/Scripts/Bonuses_interface.cs
+++
@@ -1,9 +0,0 @@
-namespace YG
-{
-    public partial interface IPlatformsYG2
-    {
-        void ConsumeBonus(string token) { }
-        void GetBonusCatalog() { }
-        void GetPlayerBonuses() { }
-    }
-}
